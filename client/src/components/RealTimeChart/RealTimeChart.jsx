3cf9197675286c9b1a84d32a012bd2c03563f362
Rafael Weiss <dataweiss@gmail.com>
2020-08-02 01:16:22 -0300

    Delaying frame rate for chart

--- client/src/components/RealTimeChart/RealTimeChart.jsx
+++ client/src/components/RealTimeChart/RealTimeChart.jsx
@@ -5,6 +5,8 @@ import {ChartContainer} from "./RealTimeChart.style";
 
 const RealTimeChart = ({stock}) => {
   const [chartInstance, setChartInstance] = useState(null);
+  const [speedReducer, setSpeedReducer] = useState(0);
+  const [frameRate, setFrameRate] = useState(10);
   const [data, setData] = useState([]);
   const {symbol, companyName, basePrice, subscribed} = stock;
   const chartContainer = useRef(null);
@@ -13,7 +15,7 @@ const RealTimeChart = ({stock}) => {
   const chartConfig = {
     type: 'line',
     data: {
-      labels: ['30s', '25s', '20s', '15s', '10s', '5s', '0s'],
+      labels: ['60s', '55s', '50s', '45s', '40s', '35s', '30s', '25s', '20s', '15s', '10s', '5s', '0s'],
       datasets: [{
         label: symbol,
         backgroundColor: colors.byneBlue,
@@ -63,7 +65,7 @@ const RealTimeChart = ({stock}) => {
     }
   }, [chartContainer]);
 
-  useEffect(() => {
+  const updateData = () => {
     const newData = [...data, price];
     if (newData.length > 20) {
       newData.shift();
@@ -72,6 +74,16 @@ const RealTimeChart = ({stock}) => {
     if(chartInstance){
       chartInstance.data.datasets[0].data = newData;
       chartInstance.update();
+    }
+  }
+
+  useEffect(() => {
+    if(speedReducer <= frameRate){
+      setSpeedReducer(speedReducer + 1)
+    }
+    if(speedReducer > frameRate){
+      setSpeedReducer(0)
+      updateData()
     }
   }, [price]);
 
